e1a024908f6a14158d17ece23db30c4987c07ddf
diogoshindhinaka <diogoshindhinaka@gmail.com>
2024-07-04 19:43:36 +0000

    aplica o ajuste na rota de sucesso

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { Home } from './pages/Home'
-import { Checkout } from './pages/Checkout'
 import { Success } from './pages/Success'
+import { Checkout } from './pages/Checkout'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
@@ -11,7 +11,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/order/:orderId/success" element={<Success />} />
       </Route>
     </Routes>
   )
